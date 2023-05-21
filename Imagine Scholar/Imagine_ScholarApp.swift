666ab21fff57c9515bab6e082944f678a8fa43d5
PhilaDlamini <phila.dlamini@tufts.edu>
2023-05-21 01:36:32 -0400

    Initial Commit

---
+++ Imagine Scholar/Imagine_ScholarApp.swift
@@ -0,0 +1,17 @@
+//
+//  Imagine_ScholarApp.swift
+//  Imagine Scholar
+//
+//  Created by Phila Dlamini on 5/21/23.
+//
+
+import SwiftUI
+
+@main
+struct Imagine_ScholarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
